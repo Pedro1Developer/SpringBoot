43f09ab2bea9b5a89952650c1c5d68644c9477d6
Pedro <sky876613@gmail.com>
2023-05-10 18:47:47 -0300

    Teste criação

---
+++ TesteCriacao/src/main/java/com/example/demo/TesteCriacaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteCriacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteCriacaoApplication.class, args);
+	}
+
+}

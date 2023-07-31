2a5413bb54194da8574dfba8415be4554ab29152
Pedro <sky876613@gmail.com>
2023-07-30 22:55:35 -0300

    second commit

---
+++ carros/src/main/java/com/carros/IndexController.java
@@ -0,0 +1,32 @@
+package com.carros;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+
+	@GetMapping
+	public String get() {
+		return "get Spring Boot";
+	}
+
+	@GetMapping("/login/{login}/senha/{senha}")
+	public String login(@PathVariable("login") String login, @PathVariable("senha") String senha) {
+		return "Login: " + login + " Senha: " + senha;
+	}
+	
+	@GetMapping("/carro/{id}")
+	public String carrosById(@PathVariable("id") Long id) {
+		return ("Carro " + id);
+	}
+	
+	@GetMapping("/carro/tipo/{tipo}")
+	public String carroByTipo(@PathVariable("tipo") String tipo) {
+		return("Lista de carros " + tipo);
+	}
+
+}
